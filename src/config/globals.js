95c83d0da8a4ff99457dbe7e658fb8ea6c7fe62e
Jesus Perez <jesusmpds18@gmail.com>
2022-01-12 17:13:15 -0300

    Fixed script command

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+if (process.env.NODE_ENV === "development") require("dotenv").config();
 
 module.exports = {
   PORT: process.env.PORT ? Number(process.env.PORT) : 8080,
